d148ef83612dabaf9345ec6ba0face779cbf5dd7
damionw <damionw@gmail.com>
2017-10-19 21:57:17 -0300

    Treeview implementation

--- src/static/functions.js
+++ src/static/functions.js
@@ -1,2 +1,68 @@
 function init() {
+    populate_tree("#panel");
+}
+
+function populate_tree(panel) {
+    function populate(treedata, d3_element, parent) {
+        for (i=0; i < treedata.length; ++i) {
+            var node = treedata[i];
+
+            if (parent != node.parent) {
+                continue;
+            }
+
+            console.log("Populating " + node.filename + " " + node.path);
+
+            var label = (
+                parent == "" ? node.filename : node.path
+            );
+
+            if (node.type == "group") {
+                var child_element = d3_element.append("details");
+
+                child_element
+                    .append("summary")
+                    .text(label)
+                ;
+
+                populate(treedata, child_element, node.path);
+            } else {
+                var child_element = d3_element.append("div");
+
+                child_element
+                    .text(label)
+                ;
+            }
+
+            child_element
+                .on("click", function() {select_content(node, this);})
+            ;
+        }
+    }
+
+    function select_content(node, element) {
+        console.log("Selecting " + node.filename + ":" + node.path);
+    }
+
+    function refresh_display(treedata, panel) {
+        var d3_element = d3.select(panel);
+
+        d3_element.html("");
+
+        populate(treedata, d3_element, "");
+    }
+
+    ajaxFunction(
+        "/api/tree/",
+
+        function(ajax_result){
+            refresh_display(ajax_result["nodes"], panel);
+        },
+
+        function(ajax_exception){
+        },
+
+        "GET"
+    );
+    
 }
